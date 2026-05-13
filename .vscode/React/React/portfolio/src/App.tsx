9478d66c6d1aed8a35978a40387ec537979d3a03
juancarlossucuzunaygualoto97@gmail.com <juancarlossucuzunaygualoto97@gmail.com>
2026-05-13 21:01:50 +0200

    3T

---
+++ .vscode/React/React/portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+// import Header from './components/Header';
+// import Header1 from './components/main/Header1';
+import { MainLayout } from './layout/MainLayout';
+
+
+function App() {
+
+  return (
+      <MainLayout/>
+      
+      // <Header1/>
+  );
+}
+
+export default App
