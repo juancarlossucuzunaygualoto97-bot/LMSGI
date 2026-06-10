01a878ab8f3ef645afd248855fe0abb5b25bf3ec
juancarlossucuzunaygualoto97@gmail.com <juancarlossucuzunaygualoto97@gmail.com>
2026-06-10 08:02:21 +0200

    Fix Supabase env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,12 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const URL = import.meta.env.VITE_SUPABASE_URL;
 const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!URL) {
-  throw new Error("VITE_SUPABASE_URL no está definida");
-}
-
-if (!KEY) {
-  throw new Error("VITE_SUPABASE_ANON_KEY no está definida");
-}
-
 export const supabase = createClient(URL, KEY);
