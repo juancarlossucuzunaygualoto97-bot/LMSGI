49fd8deff7defa5212117efc662bc60045d70988
juancarlossucuzunaygualoto97@gmail.com <juancarlossucuzunaygualoto97@gmail.com>
2026-06-10 02:31:19 +0200

    Eliminar .vscode

--- .vscode/React/React/portfolio-page/src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-
-export const Home = () => {
-    return (
-        <h1 className="min-h-screen flex items-center justify-center flex-col">Pagina principal Home</h1>
-    )
-}
--- .vscode/React/React/portfolio-page/src/pages/Trabajos.tsx
+++
@@ -1,6 +0,0 @@
-
-export const Trabajos = () => {
-    return (
-        <h1 className="min-h-screen flex items-center justify-center flex-col">Pagina principal Trabajos</h1>
-    )
-}
