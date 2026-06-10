c925638a7bca426851ba0c1221447507e42d709a
juancarlossucuzunaygualoto97@gmail.com <juancarlossucuzunaygualoto97@gmail.com>
2026-06-10 03:27:10 +0200

    fix vercel build

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Faltan variables de entorno de Supabase");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(URL, KEY);
